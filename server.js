9428cdbd98cff013e6be575525e9b9e864bc2fb3
thebuggybug <rohansayami@gmail.com>
2022-08-08 13:37:49 +0545

    worked with dummy data, learned about mongoDB and node.js, viewed data from mongo with POSTMAN

--- server.js
+++ server.js
@@ -1,14 +1,67 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
+const { MongoClient } = require("mongodb");
+
+// ---------- dummy data ----------
+
+// const articleInfo = {
+//   "learn-react": {
+//     comments: [],
+//   },
+
+//   "learn-node": {
+//     comments: [],
+//   },
+//   "learn-mongo": {
+//     comments: [],
+//   },
+// };
+// ---------- dummy data ----------
+
+// const withDB = async (operations, res) => {
+//   try {
+//     const client = await MongoClient.connect("mongodb://localhost:27017");
+//     const db = client.db("mernblog"); //DB name
+//     await operations(db);
+//     client.close();
+//   } catch (error) {
+//     res.status(500).json({ message: "Error connecting to database", error });
+//   }
+// };
 
 // Initialize Middleware
 // an express function to parse incoming JSON payload
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("hello friend"));
+// -------------- TEST ROUTRS -------------- //
+// app.get("/", (req, res) => res.send("hello friend"));
+// app.post("/", (req, res) => res.send(`Hello i am ${req.body.name}`));
+// app.get("/hello/:name", (req, res) => res.send(`Hey there ${req.body.name}`));
+// -------------- TEST ROUTRS -------------- //
 
-app.post("/", (req, res) => res.send(`Hello i am ${req.body.name}`));
+// ----- ROUTE to get data from DB -----
+app.get("/api/articles/:name", async (req, res) => {
+  try {
+    const articleName = req.params.name;
+    const client = await MongoClient.connect("mongodb://localhost:27017");
+    const db = client.db("mernblog"); //DB name
+    const articleInfo = await db
+      .collection("article")
+      .findOne({ name: articleName }); //collectionName
+    res.status(200).json(articleInfo);
+    client.close();
+  } catch (error) {
+    res.status(500).json({ message: "Error connecting to database", error });
+  }
+});
+
+app.post("/api/articles/:name/add-comments", (req, res) => {
+  const { username, text } = req.body;
+  const articleName = req.params.name;
+  articleInfo[articleName].comments.push({ username, text });
+  res.status(200).send(articleInfo[articleName]);
+});
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
 
